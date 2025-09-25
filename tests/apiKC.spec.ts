db06f7fb4504602a7f40b8f7d5de8d086e9d8554
Eddie C. <edimc2000@gmail.com>
2025-09-24 22:37:18 -0500

    added whatsapp prelim test

--- tests/apiKC.spec.ts
+++ tests/apiKC.spec.ts
@@ -1,5 +1,31 @@
 import { test, expect } from '@playwright/test'
 
+
+function buildPayload(sendingNumber: string, message: string) {
+  return {
+    ToCountry: 'CA',
+    ToState: 'MB',
+    SmsMessageSid: 'SM0e4029a2f9adc462165ea4d740894028',
+    NumMedia: '0',
+    ToCity: 'WINNIPEG',
+    FromZip: '',
+    SmsSid: 'SM0e4029a2f9adc462165ea4d740894028',
+    FromState: 'MB',
+    SmsStatus: 'received',
+    FromCity: 'WINNIPEG',
+    Body: message, // Your test message
+    FromCountry: 'CA',
+    To: '+12048004222',
+    ToZip: '',
+    NumSegments: '1',
+    MessageSid: 'PWSM0e4029a2f9adc462165ea4d740894028',
+    AccountSid: 'PWAC32307d5ff1f312ac476550f59e8c09c7',
+    From: sendingNumber, // Test phone number
+    ApiVersion: '2010-04-10'
+  };
+}
+
+
 test('API getUsersList', async ({ request }) => {
   const getUsersList = await request.get(`http://127.0.0.1:8080/api/account/locationgroup/1`,
     {
@@ -83,13 +109,13 @@ test('API TEST - CHECKIN/UPDATE - Twilio SMS webhook test valid and invalid numb
   // Twilio-like form data payload
   let message = 'Test: Playwright ' + Math.floor(Math.random() * (1000 - 1)); // Your test message
   let sendingNumber = ['+12049981157', '+12049981480', '+12049981158']; // Your test phone number
-  
+
   // Expected XML response
   const expectedResponse = [
     `<Response></Response>`,
     `<Response></Response>`,
     "{\"success\":false,\"message\":\"Number not found\"}"
-  ]; 
+  ];
 
   for (let i = 0; i < sendingNumber.length; i++) {
     const formData = buildPayload(sendingNumber[i], message);
@@ -107,35 +133,46 @@ test('API TEST - CHECKIN/UPDATE - Twilio SMS webhook test valid and invalid numb
       },
       data: params.toString()
     });
-  
-  const responseText = await response.text();
-  // console.log('Response:', responseText);
-  // console.log('WHOLE RESPONSE:', response);
-  
-  expect(responseText).toContain(expectedResponse[i]);
+
+    const responseText = await response.text();
+    // console.log('Response:', responseText);
+    // console.log('WHOLE RESPONSE:', response);
+
+    expect(responseText).toContain(expectedResponse[i]);
   }
 });
 
-function buildPayload(sendingNumber: string, message: string) {
+function buildWhatAppPayload() {
   return {
-    ToCountry: 'CA',
-    ToState: 'MB',
-    SmsMessageSid: 'SM0e4029a2f9adc462165ea4d740894028',
-    NumMedia: '0',
-    ToCity: 'WINNIPEG',
-    FromZip: '',
-    SmsSid: 'SM0e4029a2f9adc462165ea4d740894028',
-    FromState: 'MB',
-    SmsStatus: 'received',
-    FromCity: 'WINNIPEG',
-    Body: message, // Your test message
-    FromCountry: 'CA',
-    To: '+12048004222',
-    ToZip: '',
-    NumSegments: '1',
-    MessageSid: 'PWSM0e4029a2f9adc462165ea4d740894028',
-    AccountSid: 'PWAC32307d5ff1f312ac476550f59e8c09c7',
-    From: sendingNumber, // Test phone number
-    ApiVersion: '2010-04-10'
-  };
+    "messaging_product": "whatsapp",
+    "to": "639993564007",
+    "type": "template",
+    "template": {
+      "name": "hello_world",
+      "language": { "code": "en_US" }
+    }
+   
+  }
+
 }
+
+
+
+test('API TEST - CHECKIN/UPDATE - whatsapp', async ({ request }) => {
+
+
+  const formData = buildWhatAppPayload();
+
+  const response = await request.post('https://graph.facebook.com/v22.0/789323877597697/messages', {
+    headers: {
+      'Authorization': `Bearer ${process.env.W_ACCESS_TOKEN}`,
+      'content-type': 'application/json',
+      'x-twilio-signature': 'oQ/MpmBxp7nNSyCQ0hjprQbxJG4=',
+    },
+    data: formData
+  });
+
+  console.log(response.status())
+  const responseText = await response.text();
+  console.log(responseText)
+})
