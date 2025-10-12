d2542c211d80baec85dcb6b5b40814c127ace79f
Eddie C. <edimc2000@gmail.com>
2025-10-12 00:11:11 -0500

    aria snap shot

--- tests/ipad/browserContext.spec.ts
+++ tests/ipad/browserContext.spec.ts
@@ -28,6 +28,40 @@ test.describe('Browser Context', () => {
         await expect(page).toHaveScreenshot()
         await page.waitForTimeout(1000)
     })
+    test('aria matching ', async ({ page }) => {
+        await page.goto('https://www.google.com')
+        await expect(page.getByRole('navigation')).toMatchAriaSnapshot(`
+    - navigation:
+      - link "About":
+        - /url: https://about.google/?fg=1&utm_source=google-CA&utm_medium=referral&utm_campaign=hp-header
+      - link "Store":
+        - /url: https://store.google.com/CA?utm_source=hp_header&utm_medium=google_ooo&utm_campaign=GS100042&hl=en-CA
+      - link "Gmail":
+        - /url: https://mail.google.com/mail/&ogbl
+      - link "Search for Images":
+        - /url: https://www.google.com/imghp?hl=en&ogbl
+      - button "Google apps":
+        - img
+      - link "Sign in":
+        - /url: https://accounts.google.com/ServiceLogin?hl=en&passive=true&continue=https://www.google.com/&ec=futura_exp_og_so_72776762_e
+    `);
+
+
+    await expect(page.getByRole('contentinfo')).toMatchAriaSnapshot(`
+    - link "Advertising":
+      - /url: https://www.google.com/intl/en_ca/ads/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1
+    - link "Business":
+      - /url: https://www.google.com/services/?subid=ww-ww-et-g-awa-a-g_hpbfoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpbfooter&fg=1
+    - link "How Search works":
+      - /url: https://google.com/search/howsearchworks/?fg=1
+    - link "Privacy":
+      - /url: https://policies.google.com/privacy?hl=en-CA&fg=1
+    - link "Terms":
+      - /url: https://policies.google.com/terms?hl=en-CA&fg=1
+    - button "Settings"
+    `);
+    
+    })
 
 
 })
